df8c7819fa84c8f55fd53120a0c3f6e389f20a2e
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-08 00:48:02 +0530

    Add extension method for parsing html directly from string

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,13 @@
 library ani_app.globals;
 
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
+
 
 String baseUrl = "https://gogoplay4.com";
+
+extension Parser on String {
+  Document parseString() {
+    return parse(this);
+  }
+}

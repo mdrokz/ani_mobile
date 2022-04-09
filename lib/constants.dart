98c61d994f5646df132a6ef0af62060030bc898b
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-10 01:26:03 +0530

    add encryption constants & decrypt link

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,13 +1,8 @@
 library ani_app.globals;
 
-import 'package:html/dom.dart';
-import 'package:html/parser.dart';
-
-
 String baseUrl = "https://gogoplay4.com";
+String decryptionUrl = "https://gogoplay4.com/encrypt-ajax.php";
 
-extension Parser on String {
-  Document parseString() {
-    return parse(this);
-  }
-}
+String secretKey="3633393736383832383733353539383139363339393838303830383230393037";
+String iv="34373730343738393639343138323637";
+String userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36";

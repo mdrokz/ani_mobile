3db8c30ed002a42d44fccb63bc51634f9bac567a
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-10 01:27:18 +0530

    implement decryption download link methods

--- lib/scraper.dart
+++ lib/scraper.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'constants.dart' as constants;
+import 'utils.dart' as utils;
+import 'types.dart';
 
 import 'package:html/parser.dart' show parse;
 import 'package:html/dom.dart';
@@ -13,6 +15,8 @@ final httpClient = HttpClient();
 Future<List<String>> searchAnime(String name) async {
   final req = await httpClient
       .getUrl(Uri.parse("${constants.baseUrl}/search.html?keyword=$name"));
+
+  req.headers.set("User-Agent",constants.userAgent);
 
   final res = await req.close();
 
@@ -24,7 +28,11 @@ Future<List<String>> searchAnime(String name) async {
     return x.attributes["href"]!;
   }).toList();
 
-  getEpisodes(animeList[0]);
+  final x = await getEpisodes(animeList[0]);
+
+  final link = await getDpageLink(x[0]);
+
+  decryptLink("https:" + link);
 
   return animeList;
 }
@@ -38,6 +46,8 @@ Future<List<String>> getEpisodes(String animeId) async {
   final req = await httpClient
       .getUrl(Uri.parse("${constants.baseUrl}/$animeId"));
 
+  req.headers.set("User-Agent",constants.userAgent);
+
   final res = await req.close();
 
   final html = (await res.transform(utf8.decoder).join()).parseString();
@@ -49,4 +59,49 @@ Future<List<String>> getEpisodes(String animeId) async {
   }).toList();
 
   return episodeList;
+}
+
+Future<String> getDpageLink(String episodeLink) async {
+
+  final req = await httpClient
+      .getUrl(Uri.parse("${constants.baseUrl}/$episodeLink"));
+
+  req.headers.set("User-Agent",constants.userAgent);
+
+  final res = await req.close();
+
+  final html = (await res.transform(utf8.decoder).join()).parseString();
+
+  final link = html.querySelector('iframe')?.attributes["src"]!;
+
+  if(link != null) {
+    return link;
+  }
+
+  return "";
+}
+
+Future<String> decryptLink(String downloadLink) async {
+
+  final id = downloadLink.split('=')[1].split('&')[0];
+
+  final encrypted = utils.encodeToBase64(id, constants.secretKey, constants.iv);
+
+  final req = await httpClient
+      .getUrl(Uri.parse("${constants.decryptionUrl}?id=$encrypted"));
+
+  req.headers.set("User-Agent",constants.userAgent);
+  req.headers.add("X-Requested-With", "XMLHttpRequest");
+  
+  final res = await req.close();
+
+  final data = (await res.transform(utf8.decoder).join());
+
+  final base64Data = data.split(":")[1].replaceAll("}","").replaceAll("\"","").replaceAll("\\","");
+
+  final decrypted = base64.decode(base64Data);
+
+  final decryptedData = streamFromJson(utils.decode(decrypted, constants.secretKey, constants.iv));
+
+  return decryptedData.source.first.file;
 }

cc06a4e9fda226996a2e8b5db10aa19c1652dc8a
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-15 17:02:48 +0530

    replace broken encryption keys with new keys

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
 library ani_app.globals;
 
 String baseUrl = "https://gogoplay4.com";
-String decryptionUrl = "https://gogoplay4.com/encrypt-ajax.php";
+String decryptionUrl = "https://goload.pro/encrypt-ajax.php";
 
-String secretKey="3633393736383832383733353539383139363339393838303830383230393037";
-String iv="34373730343738393639343138323637";
+String secretKey="3633393736383832383733353336383139363339393232303833323735393037";
+String iv="34373836343433393639343138323637";
 String userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36";
 
 List<String> settings = [

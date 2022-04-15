ee60ff5bb85705e56328a1fce78a2fc8a28f5672
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-15 17:07:04 +0530

    replace keys yet again

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,8 +3,8 @@ library ani_app.globals;
 String baseUrl = "https://gogoplay4.com";
 String decryptionUrl = "https://goload.pro/encrypt-ajax.php";
 
-String secretKey="3633393736383832383733353336383139363339393232303833323735393037";
-String iv="34373836343433393639343138323637";
+String secretKey="3534323530333734323633343039393638353434373737363735393932343330";
+String iv="39333130363136353733343634303435";
 String userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36";
 
 List<String> settings = [

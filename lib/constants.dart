61e8097d2ec599cda80b5ed10e1441d6b568da65
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-16 07:06:34 +0530

    update `baseUrl`

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 library ani_app.globals;
 
-String baseUrl = "https://gogoplay4.com";
+String baseUrl = "https://goload.pro";
 String decryptionUrl = "https://goload.pro/encrypt-ajax.php";
 String userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36";
 

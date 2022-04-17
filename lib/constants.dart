1e8c856446170ccee01cd60c95df52c9bfc3dbf9
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-18 01:49:35 +0530

    add selectors to constants file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,17 @@
 library ani_app.globals;
 
+
+// HTTP
 String baseUrl = "https://goload.pro";
 String decryptionUrl = "https://goload.pro/encrypt-ajax.php";
 String userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36";
+
+
+// HTML selectors
+String secondKeyPath = "div[class*='videocontent-']";
+String keyPath = "body[class^='container-']";
+String ivPath = "div[class*='container-']";
+String secretValuePath = 'script[data-name="episode"]';
 
 List<String> settings = [
   "",
